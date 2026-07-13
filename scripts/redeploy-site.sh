fc9ed88ff8c997a4c62cb288c06acb24859e507a
Mohammed Hossain <momohiuddin.h@gmail.com>
2026-07-13 03:12:27 +0100

    new: redeployment script

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,2 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
+tmux kill-server
+cd "$(dirname "$0")/.."
+git fetch && git reset origin/main --hard
+
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+tmux new-session -d -s flask "cd $(pwd) && source .venv/bin/activate && export FLASK_APP=app && flask run"
