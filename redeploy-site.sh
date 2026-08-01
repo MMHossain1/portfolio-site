15fd137b18f3f7942d47dd438b8e8caa9f2e0003
Mohammed Hossain <momohiuddin.h@gmail.com>
2026-08-02 00:35:21 +0100

    update: updated redeploy-site.sh to work with docker compose start up

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,9 +2,5 @@
 
 git fetch && git reset origin/main --hard
 
-python -m venv .venv
-source .venv/bin/activate
-pip install -r ./requirements.txt
-
-systemctl restart myportfolio
-systemctl status myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
